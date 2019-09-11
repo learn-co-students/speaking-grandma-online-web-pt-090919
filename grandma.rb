e471437dbb20b009d2935f3bd665c629e598d275
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-09-11 23:45:02 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -18,8 +18,6 @@ require 'pry'
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     p "I LOVE YOU TOO PUMPKIN!"
-  # elsif phrase != phrase.uppercase
-  #   puts "HUH?! SPEAK UP, SONNY"
   elsif phrase == phrase.upcase
     p "NO, NOT SINCE 1938!"
   else
