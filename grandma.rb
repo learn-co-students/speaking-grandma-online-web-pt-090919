8a94b6c7dd164123cdd7c6ecd1835c37a7bd66c7
Brianna Jordan <bryvjordan9@gmail.com>
2019-09-12 21:45:44 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,11 @@
-def speak_to_grandma
-
-  if speak_to_grandma == 'I LOVE YOU GRANDMA!'
-    puts 'I LOVE YOU TOO PUMKIN!'
+def speak_to_grandma(phrase)
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  end
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
 
