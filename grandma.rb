cf922c7f5f816c05bb0db22bdc03bfad4911840c
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-09-25 16:33:08 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,10 @@
-speak_to_grandma
-if == .upcase 
-  "NO, NOT SINCE 1938"
-elsif == "I LOVE YOU GRANDMA!"
-  " I LOVE YOU TOO PUMPKIN"
+def speak_to_grandma(phrase)
+if "#{phrase}" == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+elsif  "#{phrase}" == "#{phrase}".upcase  
+  return "NO, NOT SINCE 1938!"
 else 
-  "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 end
+end
+
